12884e89e2d5bcfb46c98aa7a3d70b8581c4a0ba
M.Muni Satwik Reddy <munisatwik956@gmail.com>
2026-06-23 17:23:05 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,197 @@
+import http, { get } from 'node:http'
+import path from 'node:path'
+import fs from 'node:fs/promises'
+import sanitizeHtml from 'sanitize-html'
+import EventEmitter from 'node:events'
+
+
+const sightingEvents = new EventEmitter()//
+sightingEvents.on('sighting-added', createAlert)//
+
+
+function sendResponce(res,statusCode,contentType,payload){
+
+    res.statusCode=statusCode
+    res.setHeader('Content-Type',contentType)
+    res.end(payload)
+
+}
+
+function getContentType(ext){
+    const types={
+        ".js": "text/javascript",
+        ".json": "application/json",
+        ".css": "text/css",
+        ".png": "image/png",
+        ".jpg": "image/jpg",
+        ".jpeg": "image/jpeg",
+        ".gif": "image/gif",
+        ".svg": "image/svg+xml"    
+    }
+
+    return types[ext.toLowerCase()] || "text/html"
+}
+
+async function getData(){
+    try{
+        const data = await fs.readFile(path.join(__dirname,'public','data','data.json'),'utf-8')
+
+        const parsedData=JSON.parse(data)
+
+        return parsedData
+    }catch(err){
+
+        console.log(err)
+        return []
+
+    }
+}
+
+async function handleGet(res){
+    const data = await getData()
+    const content = JSON.stringify(data)
+    sendResponce(res,200,'application/json',content)
+}
+
+async function parseJSONBody(req){
+    let body = ''
+    for await (let chunk of req){
+        body += chunk
+    }
+    try{
+        return JSON.parse(body)
+    }catch(err){
+        throw new Error(`Invalid JSON format : ${err}`)
+    }
+}
+
+async function handlePost(req,res){
+
+    try{
+
+        const parsedBody = await parseJSONBody(req)
+
+        const sanitizedBody = sanitizeInput(parsedBody)
+
+        await addNewSighting(sanitizedBody)
+
+        sightingEvents.emit('sighting-added',sanitizedBody)//
+
+        sendResponce(res,201,'application/json',JSON.stringify(sanitizedBody))
+
+        console.log("POST request recived")
+
+    }catch(err){
+
+        sendResponce(res,400,'application/json',JSON.stringify({error: err}))
+        
+    }
+
+}
+
+async function addNewSighting(newSighting){
+
+    try{
+
+        const sightings=await getData()
+        sightings.push(newSighting)
+
+        const pathJSON = path.join(__dirname,'public','data','data.json')
+
+        await fs.writeFile(pathJSON,JSON.stringify(sightings,null,2),'utf-8')
+
+    }catch(err){
+        
+        throw new Error(err)
+
+    }
+
+}
+
+
+function sanitizeInput(data){
+    
+    const sanitizedData={}
+
+    for(const [key,value] of Object.entries(data)){
+        if(typeof value === 'string'){
+            sanitizedData[key]=sanitizeHtml(value,{allowedTags:[],allowedAttributes:{}})
+        }else{
+            sanitizedData[key]=value
+        }
+    }
+
+    return sanitizedData
+
+}
+
+function createAlert(sighting){
+    console.log(`Send Alert to Gost Hunters in ${sighting.location}`)
+}
+
+
+
+//console.log(import.meta)
+const PORT=process.env.PORT || 8000 //"process.env.PORT" add this when you want to publish to internet
+const __dirname=import.meta.dirname
+const __filename=import.meta.filename
+
+
+
+//see the out put
+// console.log(sanitizeHtml('h1: <h1>I am in an h1 tag</h1>'))
+// console.log(sanitizeHtml('p: <p>I am in a p tag</p>',{allowedTags:['p'],allowedAttributes:{}}))
+// console.log(sanitizeHtml('style: <style>Dam.in.a style.tag</style>'))
+// console.log(sanitizeHtml('script: <script>I am in a script tag</script>'))
+
+
+
+
+
+const server= http.createServer(async (req,res)=>{
+    //const content=fs.readFileSync(pathToResource,'utf8')(Block the code excutation until the complete file is read)
+
+    
+    //res.writeHead(200,{'ContentTtype':'text/html'})//instead of res.StatusCode,res.setHeader you can write this line(You can not modifiy headers after this line)
+
+    if(req.url.startsWith('/api')){
+        if(req.method === 'GET'){
+            return await handleGet(res)
+        }else if(req.method === 'POST'){
+            await handlePost(req,res)
+        }
+
+    }else if(!req.url.startsWith('/api')){
+        try{
+            const publicDir=path.join(__dirname,'public')
+            const pathToResource=path.join(publicDir,req.url === '/' ? "index.html" : req.url)
+
+
+            const ext = path.extname(pathToResource)//gives extension of a file
+            const content = await fs.readFile(pathToResource)
+
+            
+            sendResponce(res,200,getContentType(ext),content)
+
+
+        }catch(err){
+            if(err.code === 'ENOENT'){//ENOENT error occurs when you try to load file that doesnot exist
+                const publicDir=path.join(__dirname,'public')
+                const pathToResource=path.join(publicDir,'404.html')
+
+                const content=await fs.readFile(pathToResource)
+
+                sendResponce(res,404,'text/html',content)
+            }else{
+                sendResponce(res,500,'text/html',`<html><h1>Server Error: ${err.code}</h1></html>`)
+            }
+        }
+    }
+
+
+})
+
+
+server.listen(PORT,()=>{
+    console.log(`Connected to port : ${PORT}`)
+})
